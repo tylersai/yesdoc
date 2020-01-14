8fc611b80e232b4ab3ce26ff46521fa6c7bddb68
Sai Han <thesaihan@gmail.com>
2020-01-15 00:48:13 +0630

    Add New Emp btn

--- src/pages/EmployeeListPage.js
+++ src/pages/EmployeeListPage.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import "./EmployeeListPage.css";
 
 import Axio from "axios";
 import { API_ENDPOINT } from "../utils/Constants";
 
 const EmployeeListPage = () => {
+  const history = useHistory();
   const [empList, setEmpList] = useState([]);
 
   const fetchEmpList = async () => {
@@ -26,17 +27,26 @@ const EmployeeListPage = () => {
       <div className="row justify-content-center">
         <div className="col-md-10 col-lg-8">
           <h3 className="text-center mt-2 mb-4">List of Employees</h3>
-          <div className="list-group">
-            {empList.map(emp => (
-              <Link
-                key={emp._id}
-                to={"/employee/" + emp._id}
-                className="list-group-item list-group-item-light list-group-item-action animate-popup"
-              >
-                <h5>{emp.memberId}</h5>
-                <h6>{emp.name}</h6>
-              </Link>
-            ))}
+          <div className="row">
+            <div className="col">
+              <button onClick={() => history.push('/employee')} className="btn btn-info float-right mb-3">+ Add New</button>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <div className="list-group">
+                {empList.map(emp => (
+                  <Link
+                    key={emp._id}
+                    to={"/employee/" + emp._id}
+                    className="list-group-item list-group-item-light list-group-item-action animate-popup"
+                  >
+                    <h5>{emp.memberId}</h5>
+                    <h6>{emp.name}</h6>
+                  </Link>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       </div>
