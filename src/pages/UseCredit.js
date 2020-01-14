8b462292e5e7fc81bb53eb4fdcbaf2ef51ba7c4b
Sai Han <thesaihan@gmail.com>
2020-01-15 00:19:02 +0630

    Allow float value in amount number input box setting step='.01'

--- src/pages/UseCredit.js
+++ src/pages/UseCredit.js
@@ -79,7 +79,7 @@ const UseCredit = ({ match }) => {
                   value={amount}
                   required
                   onChange={onAmtChange}
-                  placeholder="0.00"
+                  step=".01"
                 />
                 <small id="memberIdHelp" className="form-text text-muted">
                   Enter credit amount you would like to use.
