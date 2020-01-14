59247ad499ba5181962c0ea34f5cf0c06d436219
Sai Han <thesaihan@gmail.com>
2020-01-15 05:21:41 +0630

    First Release

---
+++ build/precache-manifest.e831995592b92102d304bb243305d339.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53f2cb2d59c600517bcaa8c78ff7eb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b950635cdeed996ec2ee",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "91b25e666bd3edcc6c38",
+    "url": "/static/css/main.4947736e.chunk.css"
+  },
+  {
+    "revision": "b950635cdeed996ec2ee",
+    "url": "/static/js/2.627689d1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.627689d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "91b25e666bd3edcc6c38",
+    "url": "/static/js/main.063cf66c.chunk.js"
+  },
+  {
+    "revision": "66664d439277e587128f",
+    "url": "/static/js/runtime-main.dc542af0.js"
+  }
+]);
