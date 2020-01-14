20fe54ba832c6157df85dbefce8a8a9826fbd2d3
Sai Han <thesaihan@gmail.com>
2020-01-14 16:43:55 +0630

    Add popup and enlarge animations

--- src/pages/EmployeeDetailPage.js
+++ src/pages/EmployeeDetailPage.js
@@ -22,79 +22,87 @@ const EmployeeDetailPage = ({ match }) => {
 
   const onEmpChange = e => {
     const newEmp = Object.assign({}, emp);
-    newEmp[e.target.getAttribute('id')] = e.target.value;
+    newEmp[e.target.getAttribute("id")] = e.target.value;
     setEmp(newEmp);
   };
 
   return (
-    <div className="EmployeeDetailPage container">
+    <div className="EmployeeDetailPage container py-4">
       <div className="row justify-content-center">
-        <div className="col-md-10 col-lg-6">
-          <div className="bg-light py-4 py-sm-5 px-3 px-sm-4 px-md-5 form-wrapper shadow">
-          <form>
-            <div className="form-group">
-              <label htmlFor="memberId">Member ID : </label>
-              <input
-                type="text"
-                className="form-control"
-                id="memberId"
-                aria-describedby="memberIdHelp"
-                readOnly={match.params.id ? true : false}
-                disabled={match.params.id ? true : false}
-                value={emp.memberId}
-                required={true}
-                onChange={onEmpChange}
-              />
-              <small id="memberIdHelp" className="form-text text-muted">
-                Once Employee Member ID is set, it can't be changed!
-              </small>
-            </div>
-            <div className="form-group">
-              <label htmlFor="name">Name : </label>
-              <input
-                type="text"
-                className="form-control text-info"
-                id="name"
-                aria-describedby="nameHelp"
-                value={emp.name}
-                required={true}
-                onChange={onEmpChange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="dateOfBirth">Date of Birth : </label>
-              <input
-                type="date"
-                className="form-control text-info"
-                id="dateOfBirth"
-                aria-describedby="nameHelp"
-                value={emp.dateOfBirth}
-                onChange={onEmpChange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="employerName">Employer Name : </label>
-              <input
-                type="text"
-                className="form-control text-info"
-                id="employerName"
-                aria-describedby="employerNameHelp"
-                value={emp.employerName}
-                onChange={onEmpChange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="address">Address : </label>
-              <textarea
-                className="form-control text-info"
-                id="address"
-                aria-describedby="addressHelp"
-                value={emp.address}
-                onChange={onEmpChange}
-              />
-            </div>
-            <input type="submit" className="btn btn-info" value="Save" />
-          </form>
+        <div className="col-md-10 col-lg-8">
+          <div className="float-right credits text-info animate-enlarge" id="credits">
+            <sapn id="credit">${12585.32}</sapn>
+          </div>
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-md-10 col-lg-8">
+          <div className="bg-light my-3 py-4 py-sm-5 px-3 px-sm-4 px-md-5 form-wrapper shadow animate-popup">
+            <form>
+              <h3 className="text-center mb-3">Employee Information</h3>
+              <div className="form-group">
+                <label htmlFor="memberId">Member ID : </label>
+                <input
+                  type="text"
+                  className="form-control font-weight-bold text-secondary"
+                  id="memberId"
+                  aria-describedby="memberIdHelp"
+                  readOnly={match.params.id ? true : false}
+                  disabled={match.params.id ? true : false}
+                  value={emp.memberId}
+                  required
+                  onChange={onEmpChange}
+                />
+                <small id="memberIdHelp" className="form-text text-muted">
+                  Once Employee Member ID is set, it can't be changed!
+                </small>
+              </div>
+              <div className="form-group">
+                <label htmlFor="name">Name : </label>
+                <input
+                  type="text"
+                  className="form-control text-info"
+                  id="name"
+                  aria-describedby="nameHelp"
+                  value={emp.name}
+                  required
+                  onChange={onEmpChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="dateOfBirth">Date of Birth : </label>
+                <input
+                  type="date"
+                  className="form-control text-info"
+                  id="dateOfBirth"
+                  aria-describedby="nameHelp"
+                  value={emp.dateOfBirth}
+                  onChange={onEmpChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="employerName">Employer Name : </label>
+                <input
+                  type="text"
+                  className="form-control text-info"
+                  id="employerName"
+                  aria-describedby="employerNameHelp"
+                  value={emp.employerName}
+                  onChange={onEmpChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="address">Address : </label>
+                <textarea
+                  className="form-control text-info"
+                  id="address"
+                  aria-describedby="addressHelp"
+                  value={emp.address}
+                  onChange={onEmpChange}
+                />
+              </div>
+              <input type="submit" className="btn btn-info" value="Save" />
+            </form>
           </div>
         </div>
       </div>
